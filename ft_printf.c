15d0509edb6fbe58385881ccceecf5432cf0f0f5
rpontici <rpontici@Ponticiello-PC.>
2025-01-11 20:40:39 +0100

    bella

---
+++ ft_printf.c
@@ -0,0 +1,14 @@
+int ft_printf(const char *str, ...)
+{
+    size_t  i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == '%')
+        {
+            
+        }
+    }
+    
+}
