8cd874343121ae81653df97d67df082c2049b8b9
rpontici <rpontici@Ponticiello-PC.>
2025-01-14 00:54:39 +0100

    Iniziamo printf

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char    *str = "Così parlò Zarathustra";
+    char    c = 'R';
+    int n = 42;
+    float   f = 0.42;
+    
+    printf("%d", n);
+    return (0);
+}
