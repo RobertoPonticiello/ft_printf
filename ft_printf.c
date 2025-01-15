c432c7f727ea4a7702917d8aec110894050a25e8
Roberto nicola Ponticiello <rpontici@e4r9p3.42roma.it>
2025-01-15 18:29:00 +0100

    Piccole modifiche

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,32 @@
 #include "ft_printf.h"
 
+void    ft_check(char c, va_list x)
+{
+    if (c == 'c')
+        ft_putchar(x);
+    if (c == 's')
+        ft_putstr(x);
+    if (c == 'p')
+        ft_puthex(x);
+    if (c == 'd')
+        ft_putnbr(x);
+    if (c == 'i')
+        ft_putnbr(x);
+    if (c == 'u')
+        ft_putnbr(x); //senza segno
+    if (c == 'x')
+        /* code */
+    if (c == 'X')
+        /* code */
+    if (c == '%')
+        ft_putchar('%');
+    return (void);
+}
+
 int ft_printf(const char *str, ...)
 {
+    va_list args;
+    va_start(args, str);
     size_t  i;
 
     i = 0;
--- ft_printf.h
+++ ft_printf.h
@@ -1,7 +1,9 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
-void    ft_check(char c);
+#include <stdarg.h>
+#include <stdlib.h>
+
 int ft_printf(const char *str, ...);
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
