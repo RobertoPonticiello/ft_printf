7608d6817d2005c0256fecd680a2b6393a62d5e8
Roberto nicola Ponticiello <rpontici@e4r8p2.42roma.it>
2025-01-17 20:28:38 +0100

    bekka

--- ft_puthex.c
+++ ft_puthex.c
@@ -1,5 +1,33 @@
 #include "ft_printf.h"
 
+int ft_putnbr_hex(unsigned long n)
+{
+    char *hex = "0123456789abcdef";
+    int count = 0;
+
+    if (n >= 16) {
+        count += ft_putnbr_hex(n / 16);
+    }
+    count += ft_putchar(hex[n % 16]);
+    return count;
+}
+
+int ft_printf_ptr(va_list x)
+{
+    void    *ptr;
+    int count = 0;
+
+    ptr = (void *)va_arg(x, int);
+    if (!ptr)
+    {
+        count += ft_putstr("(nil)");
+    } else {
+        count += ft_putstr("0x");
+        count += ft_putnbr_hex((unsigned long)ptr);
+    }
+    return count;
+}
+
 int ft_puthex_dw(va_list x)
 {
     unsigned int n;
